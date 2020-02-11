426460b59eb991a59c6e6863a5c114f141a0c863
alexm <alexmsmith@sbcglobal.net>
2020-02-11 13:15:14 -0600

    added

--- PC/GUI/gui.py
+++ PC/GUI/gui.py
@@ -1,20 +1,50 @@
 import tkinter
+import threading
 from tkinter import *
 from PC.start import Start
 
-# initialize main window
-main_window = tkinter.Tk()
-main_window.title("Remote LIDAR")
-main_window.minsize(250, 100)
 
-# initialize buttons
-button_start = tkinter.Button(main_window, text="Start", command=Start.start)
-button_stop = tkinter.Button(main_window, text="Stop")
+class GUI:
 
-button_start.config(height=1, width=15)
-button_stop.config(height=1, width=15)
+    def __init__(self):
+        self.start_thread = None
+        self.stop_thread = None
 
-button_stop.pack(side=BOTTOM)
-button_start.pack(side=BOTTOM)
+        self.main_window = tkinter.Tk()
+        self.main_window.title("Remote LIDAR")
+        self.main_window.minsize(250, 100)
 
-main_window.mainloop()
+        # initialize buttons
+        button_start = tkinter.Button(self.main_window, text="Start", command=self.start)
+        button_stop = tkinter.Button(self.main_window, text="Stop")
+
+        button_start.config(height=1, width=15)
+        button_stop.config(height=1, width=15)
+
+        button_stop.pack(side=BOTTOM)
+        button_start.pack(side=BOTTOM)
+
+    def start(self):
+        self.start_thread = threading.Thread(target=Start.start)
+        self.start_thread.start()
+
+
+gui = GUI()
+gui.main_window.mainloop()
+
+# # initialize main window
+# main_window = tkinter.Tk()
+# main_window.title("Remote LIDAR")
+# main_window.minsize(250, 100)
+#
+# # initialize buttons
+# button_start = tkinter.Button(main_window, text="Start", command=Start.start)
+# button_stop = tkinter.Button(main_window, text="Stop")
+#
+# button_start.config(height=1, width=15)
+# button_stop.config(height=1, width=15)
+#
+# button_stop.pack(side=BOTTOM)
+# button_start.pack(side=BOTTOM)
+#
+# main_window.mainloop()
--- PC/start.py
+++ PC/start.py
@@ -1,15 +1,15 @@
 import paramiko
 from PC.Network.tcp_recieve import TCP_Recieve
 
+
 class Start():
 
     @staticmethod
     def start():
-
-        ssh_client =paramiko.SSHClient()
+        ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='amsmith',username='pi',password='password')
-        stdin,stdout,stderr=ssh_client.exec_command('python3 /home/pi/LIDAR/Raspberry\ Pi/Network/tcp_send.py')
-        TCP_Recieve.get_file()
+        ssh_client.connect(hostname='amsmith', username='pi', password='password')
+        stdin, stdout, stderr = ssh_client.exec_command('python3 test/blink.py')
 
-        # print(type(stdin))
+        # TCP_Recieve.get_file()
+        print(stdout.readlines())
