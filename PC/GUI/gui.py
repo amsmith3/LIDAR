2606169c55cdb0c2bed3768236719ca9cb600439
Alex <alexmsmith@sbcglobal.net>
2020-02-11 14:59:38 -0600

    added stop funtionallity

--- PC/GUI/gui.py
+++ PC/GUI/gui.py
@@ -2,9 +2,10 @@ import tkinter
 import threading
 from tkinter import *
 from PC.start import Start
+from PC.stop import Stop
 
 
-class GUI:
+class GUI():
 
     def __init__(self):
         self.start_thread = None
@@ -16,7 +17,7 @@ class GUI:
 
         # initialize buttons
         button_start = tkinter.Button(self.main_window, text="Start", command=self.start)
-        button_stop = tkinter.Button(self.main_window, text="Stop")
+        button_stop = tkinter.Button(self.main_window, text="Stop", command=self.stop)
 
         button_start.config(height=1, width=15)
         button_stop.config(height=1, width=15)
@@ -27,6 +28,10 @@ class GUI:
     def start(self):
         self.start_thread = threading.Thread(target=Start.start)
         self.start_thread.start()
+
+    def stop(self):
+        self.stop_thread = threading.Thread(target=Stop.stop)
+        self.stop_thread.start()
 
 
 gui = GUI()
